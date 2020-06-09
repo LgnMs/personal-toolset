16c4dcdade3d9c27e163f81e4581f7e295279dcd
lgn <792841450@qq.com>
2020-06-09 14:33:29 +0800

    add convertMoney

---
+++ js/convertMoney.js
@@ -0,0 +1,35 @@
+/**
+ * 将数字转换为千分位分割，用于金额显示
+ */
+
+const PRECISION = 2; // 小数点位数
+
+const convertMoney = val => {
+  if (!val) return;
+  val = val.toString();
+  const one = val.split(".")[0];
+  let two = val.split(".")[1] ? val.split(".")[1] : "00";
+  const len = one.length;
+  let oldI = 0;
+  const temp = [];
+
+  for (let i = 1; i <= len; i += 1) {
+    if (i % 3 === 0 && i !== 0) {
+      temp.push(one.substring(oldI, i));
+      oldI = i;
+    }
+  }
+
+  if (len % 3 !== 0) {
+    temp.push(one.substring(oldI, len));
+  }
+
+  if (two.length < PRECISION) {
+    for (let i = two.length; i < PRECISION; i += 1) {
+      two += '0';
+    }
+  }
+  return temp.join(",") + "." + two;
+};
+
+export default convertMoney;
